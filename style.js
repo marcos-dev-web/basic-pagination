c68cdfdc09304e86c79c29d5357f81ea3a61c9c8
Marcos Vinicius <76922074+marcos-dev-web@users.noreply.github.com>
2021-01-28 09:45:38 -0300

    update in variables

--- style.js
+++ style.js
@@ -1,19 +1,23 @@
 const buttons = document.querySelectorAll('.button');
 
+const color_click = "#3867d6";
+const color_hover = "#2d98da";
+const default_color = "#45aaf2";
+
 const addStyle = (button) => {
   button.addEventListener('click', (e) => {
-    e.target.style.backgroundColor = "#3867d6";
+    e.target.style.backgroundColor = color_click;
     setTimeout(() => {
-      e.target.style.backgroundColor = "#45aaf2";
+      e.target.style.backgroundColor = default_color;
     }, 100)
   })
 
   button.addEventListener('mouseenter', (e) => {
-    e.target.style.backgroundColor = "#2d98da";
+    e.target.style.backgroundColor = color_hover;
   })
 
   button.addEventListener('mouseout', (e) => {
-    e.target.style.backgroundColor = "#45aaf2";
+    e.target.style.backgroundColor = default_color;
   })
 }
 
