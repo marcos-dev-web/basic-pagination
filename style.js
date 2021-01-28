308ffc27c1da9e042b041c7c6176129449bfae0f
marcos-dev-web <marcospuglliesyprogramer@gmail.com>
2021-01-28 10:04:53 -0300

    refatored style.js

--- style.js
+++ style.js
@@ -1,25 +1,24 @@
-const buttons = document.querySelectorAll('.button');
+const buttons = document.querySelectorAll(".button");
 
 const color_click = "#3867d6";
 const color_hover = "#2d98da";
 const default_color = "#45aaf2";
 
 const addStyle = (button) => {
-  button.addEventListener('click', (e) => {
+  button.addEventListener("click", (e) => {
     e.target.style.backgroundColor = color_click;
     setTimeout(() => {
       e.target.style.backgroundColor = default_color;
-    }, 100)
-  })
+    }, 100);
+  });
 
-  button.addEventListener('mouseenter', (e) => {
+  button.addEventListener("mouseenter", (e) => {
     e.target.style.backgroundColor = color_hover;
-  })
+  });
 
-  button.addEventListener('mouseout', (e) => {
+  button.addEventListener("mouseout", (e) => {
     e.target.style.backgroundColor = default_color;
-  })
-}
+  });
+};
 
-buttons.forEach(button => addStyle(button));
-
+buttons.forEach((button) => addStyle(button));
